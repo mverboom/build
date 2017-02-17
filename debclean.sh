ea04d6c527da77568c5f254f24f08064591aa127
Mark Verboom <mark@verboom.net>
2017-02-17 18:10:38 +0000

    Added debclean to revert a debian system to a known base list of packages.

---
+++ debclean.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Change system back to state as defined in package list
+#
+BASE=debclean.baselist
+CUR=/tmp/debclean.curlist
+
+LOC=$( dirname ${BASH_SOURCE[0]})
+BASE=$LOC/$BASE
+
+case $1 in
+"-u")
+   echo "Are you sure you want to overwrite the base list? "
+   read answer
+   if test "$answer" = "y"
+   then
+      dpkg -l | grep "^.i" | awk '{print $2}' > $BASE
+      echo "Base list updated."
+   else
+      echo "Base list not updated."
+   fi
+;;
+"-c")
+   dpkg -l | grep "^.i" | awk '{print $2}' > $CUR
+   diff $BASE $CUR | grep '>' | cut -d' ' -f2 | xargs sudo apt-get -qq --purge -y remove
+   sudo apt-get -qq -y --purge autoremove
+   diff $BASE $CUR | grep '<' | cut -d' ' -f2 | xargs sudo apt-get -qq -y install
+   rm -f $CUR
+;;
+"-l")
+   dpkg -l | grep "^.i" | awk '{print $2}' > $CUR
+   echo "Extra packages installed:"
+   diff $BASE $CUR | grep '>' | cut -d' ' -f2
+   echo "Packages removed:"
+   diff $BASE $CUR | grep '<' | cut -d' ' -f2
+   rm -f $CUR
+;;
+*)
+   echo "Usage: debclean.sh"
+   echo "  -u  update base list"
+   echo "  -l  list difference of current packages with base list"
+   echo "  -c  revert packages to configuration in base list"
+   exit 1
+;;
+esac
+
+
